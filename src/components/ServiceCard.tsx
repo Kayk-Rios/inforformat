7bc74468e39425b77a987bd443c4d0a5ec106b80
Kayk_Darios <manjac688@gmail.com>
2025-04-15 14:27:26 -0300

    feat: :criando component Card

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,17 @@
+
+export default function ServiceCard({ title, description, icon }: any) {
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow">
+      <div className="p-6">
+        <div className="w-12 h-12 flex items-center justify-center rounded-md bg-blue-100 text-blue-800 mb-4">
+          {icon}
+        </div>
+        <h3 className="text-lg font-medium text-gray-900">{title}</h3>
+        <p className="mt-2 text-sm text-gray-500">{description}</p>
+        <div className="mt-4">
+         
+        </div>
+      </div>
+    </div>
+  );
+}
