363de7fac2a7b5f34cf3743fc0c3429da7de2627
Kayk_Darios <manjac688@gmail.com>
2025-04-15 14:34:48 -0300

    chore: colocando components no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,33 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import ContactSection from '@/components/ContactSection';
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import Navbar from '@/components/Navbar';
+import ServicesSection from '@/components/ServicesSection';
+import TestimonialsSection from '@/components/TestimonialsSection';
+import WhatsAppButton from '@/components/WhatsAppButton';
+import Head from 'next/head';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
+    <div>
+    <Head>
+        <title>Infor Format - Formatação e Serviços de Informática</title>
+        <meta name="Infor-Format" content="Serviços de formatação de computadores, instalação de Windows, Linux e pacotes Office. Atendimento especializado para empresas e residências." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-
-
-      
+      <Navbar />
+      <main>
+        <Hero />
+        <ServicesSection />
+        <TestimonialsSection/>
+        <ContactSection />
+      </main>
+      <Footer />
+      <WhatsAppButton phoneNumber="5581981699096" message="Olá! Gostaria de saber mais sobre os serviços da Infor Format." />
+   
     </div>
   );
 }
